05b433f8ee3ecbe7213351d79366d826b2318b35
Marcin Janiczek <inspiremarcin@gmail.com>
2022-02-02 00:15:35 +0100

    Initial Commit

---
+++ Login_Screen_Firebase/Login_Screen_FirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Login_Screen_FirebaseApp.swift
+//  Login_Screen_Firebase
+//
+//  Created by Marcin on 02/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct Login_Screen_FirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
